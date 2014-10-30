9ee2b2625e2a5a7edb41f86955123226b0e46173
Kei Hayashi <kei.h.hayashi@gmail.com>
2014-10-30 10:00:48 +0900

    rails g model user name:string & rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141030005621) do
+ActiveRecord::Schema.define(version: 20141030010025) do
 
   create_table "items", force: true do |t|
     t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
