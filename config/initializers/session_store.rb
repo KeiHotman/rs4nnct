4163110d04fc02ff44b1be455fd299201d435b37
Kei Hayashi <kei.h.hayashi@gmail.com>
2014-11-04 13:24:39 +0900

    change project name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_similars_session'
+Rails.application.config.session_store :cookie_store, key: '_rs4nnct_session'
